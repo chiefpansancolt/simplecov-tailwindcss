86cdb7f72d0b65b9d5d70f4af4c66b014fa80cee
Christopher Pezza <chris.pezza@hey.com>
2025-10-30 21:08:50 -0400

    Updates for gem

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./views/**/*.erb', './lib/**/*.rb'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
-}
